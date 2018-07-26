d180aed0af30a1c121d25b8e2e312d18030424cf
Joey Feng <jfeng702@gmail.com>
2018-07-26 16:15:15 -0700

    update

---
+++ levelOrder.js
@@ -0,0 +1,74 @@
+function levelOrder(root) {
+  if (!root) {
+    return null;
+  }
+
+  console.log(root.data);
+  levelOrder(root.left);
+  levelOrder(root.right);
+}
+
+
+
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  printLevelOrder() {
+    let h = this.height(root);
+    for(let i = 1; i <= h; i++) {
+      this.printGivenLevel(root, i);
+    }
+  }
+
+  height(root) {
+    if (!root) {
+      return 0;
+    } else {
+      let lheight = this.height(root.left);
+      let rheight = this.height(root.right);
+
+      if (lheight > rheight) {
+        return lheight + 1;
+      } else {
+        return rheight + 1;
+      }
+    }
+  }
+
+  printGivenLevel(root, level) {
+    if (!root) {
+      return;
+    }
+
+    if (level === 1) {
+      console.log(root.data + ' ');
+    } else if (level > 1) {
+      this.printGivenLevel(root.left, level - 1);
+      this.printGivenLevel(root.right, level - 1);
+    }
+  }
+}
+
+//    1
+// .5   2
+//       3
+
+let tree = new BinaryTree();
+tree.root = new Node(1);
+tree.root.right = new Node(2);
+tree.root.right.right = new Node(3);
+tree.root.left = new Node(.5);
+
+tree.printLevelOrder();
+// console.log(tree);
+// console.log(Object.getOwnPropertyNames(tree));
