1d2b345bebd4bab62b9aa6cbada5f71a0e6a0941
Joey Feng <jfeng702@gmail.com>
2018-08-01 16:27:08 -0700

    update

--- merge_two_lists.js
+++ merge_two_lists.js
@@ -10,29 +10,47 @@ let list2 = new ListNode(2);
 list2.next = new ListNode(4);
 list2.next.next = new ListNode(6);
 
+// recursive
 function merge(listOne, listTwo) {
-  let listThree = new ListNode(null);
-  let head = listThree;
-  while (listOne || listTwo) {
-    if (listOne && !listTwo) {
-      listThree.next = listOne;
-      listOne = listOne.next;
-    } else if (listTwo && !listOne) {
-      listThree.next = listTwo;
-      listTwo = listTwo.next;
-    } else {
-      if (listOne.val < listTwo.val) {
-        listThree.next = listOne;
-        listOne = listOne.next;
-      } else {
-        listThree.next = listTwo;
-        listTwo = listTwo.next;
-      }
-    }
-    listThree = listThree.next;
+  if (!listOne) {
+    return listTwo;
   }
-  return head.next;
+  if (!listTwo) {
+    return listOne;
+  }
+  if (listOne.val < listTwo.val) {
+    listOne.next = merge(listOne.next, listTwo);
+    return listOne;
+  } else {
+    listTwo.next = merge(listOne, listTwo.next);
+    return listTwo;
+  }
+
 }
+
+// function merge(listOne, listTwo) {
+//   let listThree = new ListNode(null);
+//   let head = listThree;
+//   while (listOne || listTwo) {
+//     if (listOne && !listTwo) {
+//       listThree.next = listOne;
+//       listOne = listOne.next;
+//     } else if (listTwo && !listOne) {
+//       listThree.next = listTwo;
+//       listTwo = listTwo.next;
+//     } else {
+//       if (listOne.val < listTwo.val) {
+//         listThree.next = listOne;
+//         listOne = listOne.next;
+//       } else {
+//         listThree.next = listTwo;
+//         listTwo = listTwo.next;
+//       }
+//     }
+//     listThree = listThree.next;
+//   }
+//   return head.next;
+// }
 
 let result = merge(list1, list2);
 console.log(result);
