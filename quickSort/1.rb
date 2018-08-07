01032ab3dade3d60f7c7b5cecf9bf2f6d6068396
Joey Feng <jfeng702@gmail.com>
2018-08-06 20:25:37 -0700

    kth largest

--- quickSort/1.rb
+++ quickSort/1.rb
@@ -17,6 +17,40 @@ def quick_sort(arr, left = 0, right = arr.length - 1)
   arr
 end
 
+def kth_smallest(arr, k, left = 0, right = arr.length - 1)
+  if k > 0 && k <= right - left + 1
+    pos = partition(arr, left, right)
+    if pos - left == k - 1
+      return arr[pos]
+    end
+
+# look to left of partition
+    if pos - left > k - 1
+      return kth_smallest(arr, k, left, pos - 1)
+    end
+# look to right of partition
+  # k = k - (pos - left + 1)
+    kth_smallest(arr, k - pos + left - 1, pos + 1, right )
+  end
+end
+
+def kth_largest(arr, k, left = 0, right = arr.length - 1)
+  if k > 0 && k <= right - left + 1
+    pivot = partition(arr, left, right)
+    if right - pivot  == k - 1
+      return arr[pivot]
+    end
+
+    if right - pivot > k - 1
+      return kth_largest(arr, k, pivot + 1, right)
+    end
+# [1,2,3,4,5]
+# if pivot is arr[2]
+# then k is k - (right - pivot + 1)
+    kth_largest(arr, k - right + pivot - 1, left, pivot - 1)
+  end
+end
+
 def partition(arr, left, right)
   pivot = right
   i = left
@@ -32,4 +66,6 @@ def partition(arr, left, right)
   i
 end
 
-p quick_sort([5,3,2,4,1])
+# p quick_sort([5,3,2,4,1])
+# p kth_smallest([5,4,3,2,1], 4)
+p kth_largest([5,4,3,2,1], 5)
