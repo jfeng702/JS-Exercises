b523e94eed4acbbcb3bd9a365884bd9c338f88e5
Joey Feng <jfeng702@gmail.com>
2018-08-10 09:01:58 -0700

    update

--- cracking/linkedlist/removedups.rb
+++ cracking/linkedlist/removedups.rb
@@ -9,6 +9,7 @@ class Node
 end
 
 
+# O(n) time
 def remove_dup(head)
   @head = head
   seen = {}
@@ -25,6 +26,28 @@ def remove_dup(head)
   @head
 end
 
+# no buffer but O(n^2) time
+
+# 1 => 2 => 2 => 7
+
+def remove_dup2(head)
+  @head = head
+  while head
+    runner = head.next
+    prev = head
+    while runner
+      if runner.val == head.val
+        prev.next = runner.next
+      else
+        prev = runner
+      end
+      runner = runner.next
+    end
+    head = head.next
+  end
+  @head
+end
+
 
 head = Node.new(1)
 head.next = Node.new(2)
@@ -32,4 +55,4 @@ head.next.next = Node.new(2)
 head.next.next.next = Node.new(2)
 head.next.next.next.next = Node.new(3)
 
-p remove_dup(head)
+p remove_dup2(head)
