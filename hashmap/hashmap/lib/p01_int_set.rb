d3ccff2c3f466e4ace5477fb25220132238eb93b
Joey Feng <jfeng702@gmail.com>
2018-08-14 13:34:06 -0700

    update

---
+++ hashmap/hashmap/lib/p01_int_set.rb
@@ -0,0 +1,104 @@
+class MaxIntSet
+  def initialize(max)
+    @store = Array.new(max)
+  end
+
+  def insert(num)
+    raise 'Out of bounds' unless is_valid?(num)
+    @store[num] = true
+  end
+
+  def remove(num)
+    @store[num] = false
+  end
+
+  def include?(num)
+    @store[num] == true
+  end
+
+  private
+
+  def is_valid?(num)
+    num < @store.length && num >= 0
+  end
+
+  def validate!(num)
+
+  end
+end
+
+
+class IntSet
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+  end
+
+  def insert(num)
+    self[num] << num
+  end
+
+  def remove(num)
+    self[num].delete(num)
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+end
+
+class ResizingIntSet
+  attr_reader :count
+
+  def initialize(num_buckets = 20)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(num)
+    resize! if num_buckets <= @count
+    self[num] << num
+    @count += 1
+  end
+
+  def remove(num)
+    self[num].delete(num)
+    @count -= 1
+  end
+
+  def include?(num)
+    self[num].include?(num)
+  end
+
+  private
+
+  def [](num)
+    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    new_store = Array.new(num_buckets * 2) { Array.new }
+    length = new_store.length
+    @store.each do |arr|
+      arr.each do |el|
+        new_store[el % length] << el
+      end
+    end
+    @store = new_store
+  end
+end
