2f14320e44f0f472438a65a1f467fff115f51fec
Joey Feng <jfeng702@gmail.com>
2018-08-14 13:42:01 -0700

    update

--- hashmap/hashmap/lib/p02_hashing.rb
+++ hashmap/hashmap/lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    count = 0
+    self.each_with_index do |e,i|
+      count += (e.ord * i)
+    end
+    count
   end
 end
 
 class String
   def hash
+    count = 0
+    self.each_char.with_index do |e,i|
+      count += (e.ord * i)
+    end
+    count
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    count = 0
+    self.keys.each do |key|
+      count += key.to_s.ord
+    end
+    count
   end
 end
