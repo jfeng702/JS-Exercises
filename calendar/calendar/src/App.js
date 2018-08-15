e698c7790699ec7718d03d2e1b518b908d43b2ca
Joey Feng <jfeng702@gmail.com>
2018-08-15 13:51:03 -0700

    update

--- calendar/calendar/src/App.js
+++ calendar/calendar/src/App.js
@@ -6,16 +6,62 @@ class App extends Component {
     this.state = {
       start: '',
       end: '',
-      date: '08/15',
+      date: '08/16',
+      currentDate: this.getDate(),
       apts: {
         '08/15': [8,9]
-      }
+      },
+      errors: {}
     };
 
     this.handleStart = this.handleStart.bind(this);
     this.handleEnd = this.handleEnd.bind(this);
     this.handleDate = this.handleDate.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleValidation() {
+    let isValid = true;
+    let errors = {};
+    const { date, currentDate, start, end, apts } = this.state;
+
+    // date has to be in future
+    // check month
+    if (date.slice(0,2) < currentDate.slice(0,2) ) {
+      isValid = false;
+      errors['date'] = 'month has to be in the future';
+    } else if (date.slice(0,2) === currentDate.slice(0,2)) {
+      // check date
+      if (date.slice(3,5) < currentDate.slice(3,5) + 1) {
+        isValid = false;
+        errors['date'] = 'date has to be in the future';
+      }
+    }
+    // time/date cannot conflict with existing appts
+    if (apts[date]) {
+      let requestedTimes = [];
+      for (let i = Number(start); i <= Number(end); i++) {
+        requestedTimes.push(i);
+      }
+      for (let time of requestedTimes) {
+        if (apts[date] && apts[date].includes(time)) {
+          isValid = false;
+          errors['time'] = 'time conflict';
+        }
+      }
+    }
+
+    console.log(errors);
+    return isValid;
+  }
+
+
+  getDate() {
+    let today = new Date();
+    let mm = today.getMonth()+1;
+    mm = (mm < 10 ? '0' + mm : mm);
+    let dd = today.getDate();
+    return mm + '/' + dd;
   }
 
   handleStart(e) {
@@ -30,22 +76,36 @@ class App extends Component {
     this.setState({date: e.target.value});
   }
 
-  validate(date, start, end) {
-    const errors = [];
-  }
 
   handleSubmit(e) {
     e.preventDefault();
-    let date = document.querySelector('.date').value;
-    let start = document.querySelector('.start').value;
-    let end = document.querySelector('.end').value;
-    let apts = Object.assign({},this.state.apts);
-    if (apts[date]) {
+    const { date, start, end, apts } = this.state;
+    // let apts = Object.assign({},this.state.apts);
+
+    if (this.handleValidation()) {
       let requestedTimes = [];
       for (let i = Number(start); i <= Number(end); i++) {
         requestedTimes.push(i);
       }
-      this.setState({ apts: [...apts[date], ...requestedTimes] }, () => console.log(this.state));
+      let priorTimes;
+      if (apts[date]) {
+        priorTimes = apts[date];
+      } else {
+        priorTimes = [];
+      }
+      this.setState(prevState => ({
+        ...prevState,
+        apts:{
+          ...prevState.apts,
+          [date]: [
+            ...priorTimes,
+            ...requestedTimes
+          ]
+        }
+      }), () => console.log(this.state));
+
+    } else {
+      alert('errors');
     }
   }
 
