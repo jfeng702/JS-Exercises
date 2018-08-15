a71f159597ebb0e1389f4ed43967ab87cd1cd217
Joey Feng <jfeng702@gmail.com>
2018-08-15 11:35:32 -0700

    update

--- calendar/calendar/src/App.js
+++ calendar/calendar/src/App.js
@@ -4,28 +4,65 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      hi: 'hey there',
-      value: ['10/15','10/16']
+      start: '',
+      end: '',
+      date: '08/15',
+      apts: {
+        '08/15': [8,9]
+      }
     };
 
-    this.handleChange = this.handleChange.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+    this.handleEnd = this.handleEnd.bind(this);
+    this.handleDate = this.handleDate.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleChange(e) {
-    this.setState({value: e.target.value});
+  handleStart(e) {
+    this.setState({start: e.target.value});
+  }
+
+  handleEnd(e) {
+    this.setState({end: e.target.value});
+  }
+
+  handleDate(e) {
+    this.setState({date: e.target.value});
+  }
+
+  validate(date, start, end) {
+    const errors = [];
   }
 
   handleSubmit(e) {
     e.preventDefault();
+    let date = document.querySelector('.date').value;
+    let start = document.querySelector('.start').value;
+    let end = document.querySelector('.end').value;
+    let apts = Object.assign({},this.state.apts);
+    if (apts[date]) {
+      let requestedTimes = [];
+      for (let i = Number(start); i <= Number(end); i++) {
+        requestedTimes.push(i);
+      }
+      this.setState({ apts: [...apts[date], ...requestedTimes] }, () => console.log(this.state));
+    }
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          Date:
-          <input type="text" value={this.state.value} onChange={this.handleChange}/>
+          Enter a date in the form mm/dd
+          <input type="text" className="date" value={this.state.date} onChange={this.handleDate} />
+        </label>
+        <label>
+          Start Time:
+          <input type="text" className="start" value={this.state.start} onChange={this.handleStart} />
+        </label>
+        <label>
+          End Time:
+          <input type="text" className="end" value={this.state.end} onChange={this.handleEnd} />
         </label>
         <input type="submit" value="submit"/>
       </form>
