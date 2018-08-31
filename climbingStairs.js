74f2d00ef31e6768545f0a0b206a536b8639b230
Joey Feng <jfeng702@gmail.com>
2018-08-31 13:46:05 -0700

    update

--- climbingStairs.js
+++ climbingStairs.js
@@ -18,16 +18,16 @@ const climbingStairs = (n, start = 0, memo = {}) => {
   if (start > n) {
     return 0;
   }
-  if (memo[start] > 0) {
+  if (memo[start]) {
     return memo[start];
   }
 
   memo[start] = climbingStairs(n, start + 1, memo) + climbingStairs(n, start + 2, memo);
-  return climbingStairs(n, start + 1) + climbingStairs(n, start + 2);
+  return memo[start];
 };
 
 console.log(climbingStairs(1));
 console.log(climbingStairs(2));
 console.log(climbingStairs(3));
 console.log(climbingStairs(4));
-console.log(climbingStairs(5));
+console.log(climbingStairs(100));
