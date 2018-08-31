e08213b8e8874c954cec1345e104838d12d12b36
Joey Feng <jfeng702@gmail.com>
2018-08-31 13:28:23 -0700

    update

--- climbingStairs.js
+++ climbingStairs.js
@@ -1,11 +1,28 @@
 // 1 or 2 steps
 
-const climbingStairs = (n, start = 0) => {
+// brute force
+// const climbingStairs = (n, start = 0) => {
+//   if (start === n) {
+//     return 1;
+//   } else if (start > n) {
+//     return 0;
+//   }
+//   return climbingStairs(n, start + 1) + climbingStairs(n, start + 2);
+// };
+
+const climbingStairs = (n, start = 0, memo = {}) => {
+
   if (start === n) {
     return 1;
-  } else if (start > n) {
+  }
+  if (start > n) {
     return 0;
   }
+  if (memo[start] > 0) {
+    return memo[start];
+  }
+
+  memo[start] = climbingStairs(n, start + 1, memo) + climbingStairs(n, start + 2, memo);
   return climbingStairs(n, start + 1) + climbingStairs(n, start + 2);
 };
 
