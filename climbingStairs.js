1c50bf3c3eb52cd501554df06178e08717115b01
Joey Feng <jfeng702@gmail.com>
2018-09-07 15:24:19 -0700

    update

--- climbingStairs.js
+++ climbingStairs.js
@@ -26,18 +26,18 @@
 //   return memo[start];
 // };
 
-const climbingStairs = (n) => {
-  if (n === 1) {
-    return 1;
-  }
-  const dp = [];
-  dp[1] = 1;
-  dp[2] = 2;
-  for (let i = 3; i <= n; i++) {
-    dp[i] = dp[i - 1] + dp[i - 2];
-  }
-  return dp[n];
-};
+// const climbingStairs = (n) => {
+//   if (n === 1) {
+//     return 1;
+//   }
+//   const dp = [];
+//   dp[1] = 1;
+//   dp[2] = 2;
+//   for (let i = 3; i <= n; i++) {
+//     dp[i] = dp[i - 1] + dp[i - 2];
+//   }
+//   return dp[n];
+// };
 
 console.log(climbingStairs(1));
 console.log(climbingStairs(2));
