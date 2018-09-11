1991fe677464e299314555aa78203a9df6e7367a
Joey Feng <jfeng702@gmail.com>
2018-09-11 14:11:54 -0700

    update

---
+++ DP2/firstname_lastname/lib/dynamic_programming.rb
@@ -0,0 +1,45 @@
+class DynamicProgramming
+
+  def initialize
+    @blair_cache = {1 => 1, 2 => 2}
+  end
+
+  def blair_nums(n)
+    return @blair_cache[n] if @blair_cache[n]
+    ans = blair_nums(n-1) + blair_nums(n-2) + ((n - 2) * 2 + 1)
+    @blair_cache[n] = ans
+    ans
+  end
+
+  def frog_hops_bottom_up(n)
+
+  end
+
+  def frog_cache_builder(n)
+
+  end
+
+  def frog_hops_top_down(n)
+
+  end
+
+  def frog_hops_top_down_helper(n)
+
+  end
+
+  def super_frog_hops(n, k)
+
+  end
+
+  def knapsack(weights, values, capacity)
+
+  end
+
+  # Helper method for bottom-up implementation
+  def knapsack_table(weights, values, capacity)
+
+  end
+
+  def maze_solver(maze, start_pos, end_pos)
+  end
+end
