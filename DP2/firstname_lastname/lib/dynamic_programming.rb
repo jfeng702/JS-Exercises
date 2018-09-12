e6beeeabef076a374d7012257deac111da582ef9
Joey Feng <jfeng702@gmail.com>
2018-09-12 09:36:22 -0700

    update

--- DP2/firstname_lastname/lib/dynamic_programming.rb
+++ DP2/firstname_lastname/lib/dynamic_programming.rb
@@ -2,6 +2,10 @@ class DynamicProgramming
 
   def initialize
     @blair_cache = {1 => 1, 2 => 2}
+    @frog_cache = {
+      1 => [[1]],
+      2 => [[1,1],[2]],
+      3 => [[3],[1,2],[2,1],[1,1,1]] }
   end
 
   def blair_nums(n)
@@ -12,19 +16,54 @@ class DynamicProgramming
   end
 
   def frog_hops_bottom_up(n)
-
+    cache = frog_cache_builder(n)
+    cache[n]
   end
 
   def frog_cache_builder(n)
+    frog_cache = {
+      1 => [[1]],
+      2 => [[1,1],[2]],
+      3 => [[3],[1,2],[2,1],[1,1,1]] }
 
+    return frog_cache if n < 4
+
+    (4..n).each do |k|
+      current_set = []
+      frog_cache[k-1].each do |array|
+        current_set << (array.dup << 1)
+      end
+
+      frog_cache[k-2].each do |array|
+        current_set << (array.dup << 2)
+      end
+
+      frog_cache[k-3].each do |array|
+        current_set << (array.dup << 3)
+      end
+      frog_cache[k] = current_set
+    end
+    frog_cache
   end
 
   def frog_hops_top_down(n)
+    @frog_cache = {
+      1 => [[1]],
+      2 => [[1,1],[2]],
+      3 => [[3],[1,2],[2,1],[1,1,1]] }
 
+    frog_hops_top_down_helper(n)
   end
 
   def frog_hops_top_down_helper(n)
+    return @frog_cache[n] if @frog_cache[n]
+    ans =
+      frog_hops_top_down_helper(n-1).map {|arr| arr.dup << 1} +
+      frog_hops_top_down_helper(n-2).map {|arr| arr.dup << 2} +
+      frog_hops_top_down_helper(n-3).map {|arr| arr.dup << 3}
 
+    @frog_cache[n] = ans
+    ans
   end
 
   def super_frog_hops(n, k)
